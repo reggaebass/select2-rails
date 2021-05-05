6ec3e7255f5028c73b562a543e8c37bc0a428c2e
reggaebass <marcelliotsmith@gmail.com>
2021-05-05 14:20:27 -0400

    test new idea 2

--- vendor/assets/javascripts/select2.js
+++ vendor/assets/javascripts/select2.js
@@ -2125,6 +2125,7 @@ S2.define('select2/selection/search',[
       self._keyUpPrevented = evt.isDefaultPrevented();
 
       var key = evt.which;
+      if(key == undefined) return;
 
       if (key === KEYS.BACKSPACE && self.$search.val() === '') {
         var $previousChoice = self.$selection
@@ -2187,6 +2188,7 @@ S2.define('select2/selection/search',[
         }
 
         var key = evt.which;
+        if(key == undefined) return;
 
         // We can freely ignore events from modifier keys
         if (key == KEYS.SHIFT || key == KEYS.CTRL || key == KEYS.ALT) {
@@ -5758,6 +5760,7 @@ S2.define('select2/core',[
 
     this.on('keypress', function (evt) {
       var key = evt.which;
+      if(key == undefined) return;
 
       if (self.isOpen()) {
         if (key === KEYS.ESC || key === KEYS.TAB ||
